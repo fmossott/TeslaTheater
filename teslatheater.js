5086e6324229066d83b0f5e875a8bf9ffe1447d4
Franco Mossotto <fmossott@users.noreply.github.com>
2022-01-04 21:54:59 +0100

    multisite with json

---
+++ teslatheater.js
@@ -0,0 +1,33 @@
+function loadSites(callback) {
+  $.getJSON('sites.json', sites => {
+    console.log(sites);
+    callback(sites);
+  });
+}
+
+
+function open() {
+  window.location.href = "https://www.youtube.com/redirect?q="+$(this).attr("link");
+}
+
+function buildPage(sites) {
+  const $apps = $('#apps');
+
+  $apps.children(".button").remove();
+
+  const $template = $($('#site-template').html());
+
+  sites.forEach(site => {
+    const $app = $template.clone();
+    $app.attr('id', site.id);
+    $app.attr('link', site.url);
+    $app.find('img').attr('src', `images/${site.img}`).attr('alt', site.name);
+    $app.on("click", open);
+    $app.appendTo($apps);
+  });
+
+}
+
+$(document).ready(function() {
+  loadSites(sites => buildPage(sites));
+});
